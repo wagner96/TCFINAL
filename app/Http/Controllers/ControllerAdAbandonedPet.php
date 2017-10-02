70e3fec38836fa89cde2ab6fe7794df066d6271e
wagner96 <31296459+wagner96@users.noreply.github.com>
2017-10-02 11:07:24 -0300

    02/10/2017

---
+++ app/Http/Controllers/ControllerAdAbandonedPet.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace TC\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TC\Models\AdPetAbandoned;
+use TC\Models\Pet;
+use TC\Models\PhotosPet;
+
+class ControllerAdAbandonedPet extends Controller
+{
+
+    //
+    public function index()
+    {
+        return view('admin.advertisings.allAdAbandoned');
+    }
+
+    public function create()
+    {
+        return view('admin.advertisings.createAdAbandoned');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pet = new Pet();
+
+        $data = $request->all();
+        $data['fkUser'] = auth()->user()->id;
+        $data2 = $pet->create($data);
+
+        $data = $request->all();
+
+        $adAbandoned = new AdPetAbandoned();
+        $data['fkPet']=$data2['id'];
+        $adAbandoned->create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        //echo "<pre>";
+        //  print_r($user);
+        //echo "</pre>";
+        return view('show', array('user' => $user));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function post_upload(Request $request)
+    {
+
+
+        $image = $request->file('file');
+        $imageName = time() . $image->getClientOriginalName();
+        $image->move(public_path('images'), $imageName);
+
+        $photo = new PhotosPet();
+        $photo->url = $imageName;
+       // $photo->save();
+
+
+        return response()->json(['success' => $imageName]);
+
+    }
+
+}
---
+++ public/js/all.js
@@ -0,0 +1,35 @@
+$( document ).ready(function(){
+
+    $(document).ready(function ($) {
+        $("#phone").mask("(99)9 9999-9999");
+        $("#cep").mask("99999-999");
+
+    });
+    $(document).ready(function () {
+        var date_input = $('input[name="birth_date"]'); //our date input has the name "date"
+        var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+        var options = {
+            format: 'dd/mm/yyyy',
+            container: container,
+            todayHighlight: true,
+            autoclose: true,
+        };
+        date_input.datepicker(options);
+    });
+    $(document).ready(function() {
+        $('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            $('#rootwizard .progress-bar').css({width:$percent+'%'});
+        }});
+    });
+    function sub() {
+        document.getElementById('form').submit();
+    }
+
+    Dropzone.options.imageUpload = {
+        maxFilesize: 1,
+        acceptedFiles: ".jpeg,.jpg,.png,.gif"
+    };
+});
